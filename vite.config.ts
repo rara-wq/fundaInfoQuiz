0031fea4d646d108dfdde306e1fe76d42a121fe5
rara <nr.ys.ek2676@gmail.com>
2025-07-16 15:32:19 +0900

    Update vite.config.ts for GitHub Pages base URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ここにあなたのフォークしたリポジトリ名を正確に入力してください
+const repoName = 'fundaInfoQuiz'; // 例: 'my-forked-repo'
+
 export default defineConfig({
   plugins: [react()],
+  // ここに 'base' オプションを追加
+  base: `/${repoName}/`, // リポジトリ名に合わせて変更
+
 })
